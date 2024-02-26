bf0d7e79919916e612bd1eb123638f2ad0613062
YuliaSenyshyn <150955391+YuliaSenyshyn@users.noreply.github.com>
2024-02-26 21:04:35 +0200

    lab_1_level_2_with_unittest
    I did lab1, level2 with unittest

---
+++ unittest_lab1.py
@@ -0,0 +1,36 @@
+import unittest
+
+from AlgoLab1 import gardener_bot_work
+
+
+class TestGardenerBotWork(unittest.TestCase):
+
+    def test_equal_m_n_5(self):
+        our_list = [[1, 2, 3, 4, 5],
+                    [6, 7, 8, 9, 10],
+                    [11, 12, 13, 14, 15],
+                    [16, 17, 18, 19, 20],
+                    [21, 22, 23, 24, 25]]
+        expected_output = [1, 2, 3, 4, 5, 10, 9, 8, 7, 6, 11, 12, 13, 14, 15, 20, 19, 18, 17, 16, 21, 22, 23, 24, 25]
+        self.assertEqual(gardener_bot_work(our_list), expected_output)
+
+    def test_m_2_n_4(self):
+        our_list = [[1, 2, 3, 4],
+                    [5, 6, 7, 8]]
+        expected_output = [1, 2, 3, 4, 8, 7, 6, 5]
+        self.assertEqual(gardener_bot_work(our_list), expected_output)
+
+    def test_n_1(self):
+        our_list = [[1]]
+        expected_output = [1]
+        self.assertEqual(gardener_bot_work(our_list), expected_output)
+
+    def test_m_6(self):
+        our_list = [[1], [2], [3], [4], [5], [6]]
+        expected_output = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(gardener_bot_work(our_list), expected_output)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
